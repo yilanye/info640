6222d3d3ff5c62363d25fdea08b2173f144c893c
Yilan Ye <54606078+yilanye@users.noreply.github.com>
2019-09-03 20:45:43 -0400

    Add files via upload

---
+++ Introduction-Yilan.R
@@ -0,0 +1,21 @@
+my_num<-2/2
+my_char<-"hi!"
+my_bool<-TRUE
+
+
+typeof(my_num)
+typeof(my_char)
+typeof(my_bool)
+
+my_nan<-0/0
+my_nan
+typeof(my_nan)
+
+install.packages("tidyverse")
+
+library(tidyverse)
+
+#creates a null variable
+my_nan<-0/0
+
+help(type)
