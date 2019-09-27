84afa39f52da8479681f984c20d1e3154adecaab
Yilan Ye <54606078+yilanye@users.noreply.github.com>
2019-09-26 23:53:35 -0400

    Add files via upload
    EDA Report

--- EDASuicide-Yilan.R
+++ EDASuicide-Yilan.R
@@ -17,6 +17,7 @@ head(scd)
 #SCD and POP in USA
 scd_USA <-scd %>% filter(country=="United States of America")
 head(scd_USA)
+summary(scd_USA) 
 ggplot(scd_USA,aes(x=year,y=suicides_no,color=age))+geom_jitter(alpha=.6)+expand_limits(y=0)+facet_grid(.~sex)+labs(title="Suicide Number in USA")
 ggplot(scd_USA,aes(x=year,y=population,color=age))+geom_jitter(alpha=.6)+expand_limits(y=0)+facet_grid(.~sex)+labs(title="Population in USA")
 sum(is.na(scd_USA))
@@ -36,16 +37,19 @@ head(scd_USA_mid)
 sum(is.na(scd_USA_mid))
 glimpse(scd_USA_mid)
 tail(scd_USA_mid)
+scd_USA_mid_f<-scd %>% filter(country=="United States of America") %>% filter(age=="35-54 years") %>% filter(sex=="female")
+summary(scd_USA_mid_f)
+scd_USA_mid_m<-scd %>% filter(country=="United States of America") %>% filter(age=="35-54 years") %>% filter(sex=="male")
+summary(scd_USA_mid_m)
+
 
 ggplot(scd_USA_mid,aes(x=year,y=suicides_no,color=sex))+geom_jitter(alpha=.6)+expand_limits(y=0)+labs(title="People Age of 35-54 Suicide Number in USA")
 ggplot(scd_USA_mid,aes(x=year,y=population,color=sex))+geom_jitter(alpha=.6)+expand_limits(y=0)+labs(title="Population in USA")
 
 #SCD and POP in Japan
 scd_JP <- scd %>% filter(country=="Japan") 
+summary(scd_JP) 
 head(scd_JP)
-head(scd_JP)%>%arrange(desc(suicides_no))
-
-scd_JP <- scd %>% filter(country=="Japan") %>% filter(sex=="male")%>% filter(year=="2003")
 head(scd_JP)%>%arrange(desc(suicides_no))
 
                        
@@ -67,37 +71,9 @@ scd_JP_mid<-scd %>% filter(country=="Japan") %>% filter(age=="35-54 years")
 head(scd_JP_mid)
 ggplot(scd_JP_mid,aes(x=year,y=suicides_no,color=sex))+geom_jitter(alpha=.6)+expand_limits(y=0)+labs(title="People Age of 35-54 Suicide Number in Japan")
 ggplot(scd_JP_mid,aes(x=year,y=population,color=sex))+geom_jitter(alpha=.6)+expand_limits(y=0)+labs(title="Population in Japan")
-
-#other manipulations
-scd %>% mutate(Mpop= population/1000000)
-scd %>% mutate(scdMpop=suicides_no/population*100)
-
-
-scd_2000 <-scd %>% filter(year==2000)
-head(scd_2000,n=15)
-
-scd_2000_f<-scd %>% filter(year==2000)%>%filter(sex=="female")
-head(scd_2000_f,n=15)
-
-scd_2000 %>% arrange(desc(suicides_no))
-scd_2000 %>% arrange(desc(population))%>% group_by(age,sex)
-
-
-scd_2000%>% summarise(sumscd=sum(suicides_no))
-
-scd_USA_m_mid<-scd %>% filter(country=="United States of America") %>% filter(sex=="male") %>% filter(age=="35-54 years")
-head(scd_USA_m_mid)
-
-ggplot(scd_USA_m_mid,aes(x=year,y=suicides_no))+geom_point()+expand_limits(y=0)
-
-scd_USA_m <- scd %>% filter(country=="United States of America") %>% filter(sex=="male")
-head(scd_USA_m)
-
-ggplot(scd_USA_m,aes(x=year,y=suicides_no,color=age))+geom_jitter(alpha=.6)+expand_limits(y=0)+labs(title="Male Suicide Number in USA")
-
-scd_JP_m <- scd %>% filter(country=="Japan") %>% filter(sex=="male")
-head(scd_JP_m)
-
-ggplot(scd_JP_m,aes(x=year,y=suicides_no,color=age))+geom_jitter(alpha=.6)+expand_limits(y=0)+labs(title="Male Suicide Number in Japan")
+scd_JP_mid_f<-scd %>% filter(country=="Japan") %>% filter(age=="35-54 years") %>% filter(sex=="female")
+summary(scd_JP_mid_f)
+scd_JP_mid_m<-scd %>% filter(country=="Japan") %>% filter(age=="35-54 years") %>% filter(sex=="male")
+summary(scd_JP_mid_m)
 
 
